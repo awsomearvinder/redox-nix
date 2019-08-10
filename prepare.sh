f772f4b7cb70caa3206b23334f38246ac2f229f7
jD91mZM2 <me@krake.one>
2019-08-10 17:21:32 +0200

    Update Redox

--- prepare.sh
+++ prepare.sh
@@ -10,9 +10,13 @@ dontOverrideCC() {
 
     find . -name "*.mk" -exec sed -i "s/\<$regex\>/_\1/g" "{}" \;
 }
-
 dontUseBinaries() {
-    sed -i "s/^PREFIX_BINARY\?=.*$/PREFIX_BINARY?=0/" config.mk
+    sed -i "s/^PREFIX_BINARY?=.*$/PREFIX_BINARY?=0/" config.mk
+    sed -i "s/^PREFIX_RUSTC?=.*$/PREFIX_RUSTC?=0/" config.mk
+}
+fixXargoDeadlock() {
+    # Not nix-specific and will probably be fixed soon
+    sed -i "s/export CARGO=xargo/export CARGO=\"env -u CARGO xargo\"/" prefix.mk
 }
 
 cd "$(dirname "$0")"
@@ -20,4 +24,5 @@ cd "$(dirname "$0")"
 pushd "redox/mk" > /dev/null
 dontOverrideCC
 dontUseBinaries
+fixXargoDeadlock
 popd > /dev/null
