1faac5d65cdd48023e5b90ba4c531f3ed9fe430d
jD91mZM2 <me@krake.one>
2020-05-10 15:56:12 +0200

    Remove outdated xargo deadlock fix

--- prepare.sh
+++ prepare.sh
@@ -14,15 +14,10 @@ dontUseBinaries() {
     sed -i "s/^PREFIX_BINARY?=.*$/PREFIX_BINARY?=0/" config.mk
     sed -i "s/^PREFIX_RUSTC?=.*$/PREFIX_RUSTC?=0/" config.mk
 }
-fixXargoDeadlock() {
-    # Not nix-specific and will probably be fixed soon
-    sed -i "s/export CARGO=xargo/export CARGO=\"env -u CARGO xargo\"/" prefix.mk
-}
 
 cd "$(dirname "$0")"
 
 pushd "redox/mk" > /dev/null
 dontOverrideCC
 dontUseBinaries
-fixXargoDeadlock
 popd > /dev/null
