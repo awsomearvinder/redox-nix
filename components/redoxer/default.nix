972fd6ff3bd57567b288325e071712f4e90addb1
awesomearvinder <ArvinderDhan@gmail.com>
2021-07-13 16:36:14 -0500

    Include old nativeBuildInputs when declaring new ones (components/redoxer)
    Redoxer failed to build due to it's builder not finding rustc. This was
    caused by the old nativeBuildInputs not being included when you set the
    new ones, and one of the things being tossed out was rust.

--- components/redoxer/default.nix
+++ components/redoxer/default.nix
@@ -9,13 +9,13 @@ let
     (toString ~/.redoxer/toolchain)
     root.redoxfs
   ];
-in crates.rootCrate.build.overrideAttrs (_old: {
+in crates.rootCrate.build.overrideAttrs (old: {
   src = config.srcFor "redoxer" (fetchgit {
     url = "https://gitlab.redox-os.org/redox-os/redoxer";
     rev = "0.2.19";
     sha256 = "0lakv5qg1qz0k0iw6p7i8mp97gbkiwn2723a0c1kiv7llaznv4l5";
   });
-  nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = old.nativeBuildInputs ++ [ makeWrapper ];
   postInstall = ''
     wrapProgram $out/bin/redoxer \
       `# add necessary runtime PATH` \
